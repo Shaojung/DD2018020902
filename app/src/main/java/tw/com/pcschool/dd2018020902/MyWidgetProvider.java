a0273c1169b9a6d7dadbae4dbc4050e2c96ad930
Teacher <shaojung@gmail.com>
2018-02-09 11:14:51 +0800

    Initial commit

---
+++ app/src/main/java/tw/com/pcschool/dd2018020902/MyWidgetProvider.java
@@ -0,0 +1,10 @@
+package tw.com.pcschool.dd2018020902;
+
+import android.appwidget.AppWidgetProvider;
+
+/**
+ * Created by Student on 2018/2/9.
+ */
+
+public class MyWidgetProvider extends AppWidgetProvider {
+}
